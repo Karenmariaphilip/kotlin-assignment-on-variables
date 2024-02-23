2415c85870167ceb7be926677d34faed707f93eb
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-02-23 16:14:42 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main(){
+  println("hello Philip")
+    modulas(10, 3)
+    modulas(5, 6)
+    println("I love socializing and staying humble always")
+
+}
+
+fun printhello(name: String){
+    println(name)
+
+}
+fun modulas(num1:Int, num2:Int,):Int {
+    var modulus = num1 / num2
+    println(modulus)
+    return (modulus)
+}
+fun sum(num1:Int, num2:Int, num3:Int, num4:Int):Int{
+    var sum = num1 + num2 +num3 +num4
+    println(sum)
+    return(sum)
+}
+fun print(name: String){
+    println(name)
+}
+
